e185526da03f8e90b1e60a9d35870f2dd6e41c63
laster13 <patrick@lastergeek.com>
2018-06-23 16:51:26 +0200

    Add files via upload

---
+++ leadd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -xv
+
+# Add Let's encrypt validation code to DNSSEC zone
+# v.20180325 2018 (c) Max Kostikov http://kostikov.co e-mail: max@kostikov.co
+#
+
+nsddir="/mnt/docker/nsd"
+
+maindom=`echo $CERTBOT_DOMAIN | sed -r 's/.*\.([^.]+\.[^.]+)$/\1/'`
+subdom=`echo $CERTBOT_DOMAIN | sed -r 's/(.+)\.[^.]+\.[^.]+$/\1/'`
+
+if [ "$maindom" = "$subdom" ]
+then
+        str="_acme-challenge    IN TXT \"$CERTBOT_VALIDATION\""
+else
+        str="_acme-challenge.$subdom    IN TXT \"$CERTBOT_VALIDATION\""
+fi
+
+zone="${nsddir}/zones/db.${maindom}"
+echo $str >> $zone
+
+${nsddir}/dnsnewserial.sh $zone
+
+docker exec -ti nsd nsd-control reload $maindom
+
+sleep 10
