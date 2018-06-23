6baa83ae2a29dc7396251a0ce0a60c92b7d26dfb
laster13 <patrick@lastergeek.com>
2018-06-23 16:52:46 +0200

    Add files via upload

---
+++ dnsnewserial.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -xv
+
+# Change DNS zone serial in YYYYMMDDNN format
+# v.20180401 (c)2016-18 by Max Kostikov http://kostikov.co e-mail: max@kostikov.co
+#
+# Usage: dnsnewserial.sh /path/to/zone/file
+#
+
+# Check arguments
+if [ $# -ne 1 ]
+then
+        echo "Provide /mnt/docker/nsd/zones/db.lastharo.pw as argument!"
+        exit 1
+fi
+
+# is zone file exist?
+if [ ! -f $1 ]
+then
+        echo "Zone file $1 not found!"
+        exit 1
+fi
+
+# get current serial
+curser=`cat $1 | awk -v RS='@' '$1 == "IN" && $2 == "SOA" { print $6 }'`
+
+if [ -z "$curser" ]
+then
+        echo "Serial not found in $1 file!"
+        exit 1
+fi
+
+newser=`date '+%Y%m%d'`
+
+if [ `expr $curser : $newser` = 8 ]     # if serial today was already changed
+then
+        newser=`expr $curser + 1`       # increment it
+else
+        newser=${newser}00              # else assign new
+fi
+
+# write new serial to zone file
+sed -i.bak "s/$curser/$newser/" $1
+
+# print new serial as confirmation
+echo $newser
---
+++ ledel.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -xv
+
+# Delete Let's encrypt validation code from DNSSEC zone
+# v.20180325 2018 (c) Max Kostikov http://kostikov.co e-mail: max@kostikov.co
+#
+
+nsddir="/mnt/docker/nsd"
+
+zone="${nsddir}/zones/db.${CERTBOT_DOMAIN}"
+
+[ `grep "acme-challenge" $zone | wc -l` -eq 0 ] && exit
+
+sed -i.bak '/^_acme-challenge/d' $zone
+
+${nsddir}/dnsnewserial.sh $zone
+
+docker exec -ti nsd nsd-control reload $CERTBOT_DOMAIN
