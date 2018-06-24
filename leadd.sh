0e6231566186450a0f37982e5f47882bac594396
laster13 <patrick@lastergeek.com>
2018-06-24 15:29:46 +0200

    Update leadd.sh

--- leadd.sh
+++ leadd.sh
@@ -20,7 +20,7 @@ zone="${nsddir}/zones/db.${maindom}"
 echo $str >> $zone
 
 ${nsddir}/dnsnewserial.sh $zone
-
+docker exec -ti nsd signzone $maindom [YYYYMMDDhhmmss]
 docker exec -ti nsd nsd-control reload $maindom
 
 sleep 10
