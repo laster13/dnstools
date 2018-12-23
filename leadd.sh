5eaf6a097b24fe48079d2acc491d06230ab0a91e
laster13 <patrick@lastergeek.com>
2018-12-23 15:15:00 +0100

    Add files via upload

---
+++ leadd.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Add Let's encrypt validation code to DNSSEC zone
+# v.20180325 2018 (c) Max Kostikov http://kostikov.co e-mail: max@kostikov.co
+#
+
+nsddir="/mnt/docker/nsd"
+
+maindom=`echo $CERTBOT_DOMAIN | sed -r 's/.*\.([^.]+\.[^.]+)$/\1/'`
+subdom=`echo $CERTBOT_DOMAIN | sed -r 's/(.+)\.[^.]+\.[^.]+$/\1/'`
+
+if [ "$maindom" = "$subdom" ]
+then
+        str="_acme-challenge    IN TXT \"$CERTBOT_VALIDATION\""
+else
+        str="_acme-challenge.$subdom    IN TXT \"$CERTBOT_VALIDATION\""
+fi
+
+zone="${nsddir}/zones/db.${maindom}"
+echo $str >> $zone
+
+/usr/local/bin/dnsnewserial.sh $zone
+docker exec nsd signzone $maindom
+
+sleep 10
---
+++ ledel.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Delete Let's encrypt validation code from DNSSEC zone
+# v.20180325 2018 (c) Max Kostikov http://kostikov.co e-mail: max@kostikov.co
+#
+
+nsddir="/mnt/docker/nsd"
+
+zone="${nsddir}/zones/db.${CERTBOT_DOMAIN}"
+
+[ `grep "acme-challenge" $zone | wc -l` -eq 0 ] && exit
+
+sed -i.bak '/^_acme-challenge/d' $zone
+
+/usr/local/bin/dnsnewserial.sh $zone
+docker exec nsd signzone $CERTBOT_DOMAIN
